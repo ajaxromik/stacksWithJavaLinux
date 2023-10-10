80b2e6a2091e45170f780e3b971239bbe2c9a338
ajaxromik <awillcarr@gmail.com>
2023-10-10 19:13:35 -0400

    added 2 helper methods and pushOrder first ver

--- Lab5P2Driver.java
+++ Lab5P2Driver.java
@@ -28,8 +28,7 @@ public class Lab5P2Driver {
         Bag<Package> deliveryBag = new Bag(myStack); //TODO make sure this doesn't have them work on the same thing
         Bag<Sample> sampleBag = new Bag(myStack); // i don't think it should since java passes by value
 
-        System.out.println(" --- "+args[0]+" ver. --- "
-                           +"\nSelect from the following menu:\n"
+        System.out.println("\nSelect from the following menu:\n"
                            +"\t0. Exit program\n"
                            +"\t\t1. Pick up order\n"
                            +"\t\t2. Drop off order\n"
@@ -54,7 +53,7 @@ public class Lab5P2Driver {
                 dropOrder(deliveryBag, sampleBag);
                 break;
             case 3:
-                bagInfo(deliveryBag);
+                bagInfo("Delivery",deliveryBag);
                 break;
             case 4:
                 bagInfo(sampleBag);
@@ -87,15 +86,49 @@ public class Lab5P2Driver {
         return inputDouble;
     }
 
-    public static void pushOrder(Bag<Sample> b) throws IOException{
+    private static int readInt(String prompt) {
+        System.out.print(prompt);
+        int inputDouble;
+        try {
+            inputDouble = Integer.parseInt(stdin.readLine().trim());
+            System.out.print(inputDouble);
+        } catch(Exception ex) {
+            inputDouble = 0;
+        }
+        return inputDouble;
+    }
+
+    public static void pushOrder(Bag b) throws IOException{
         System.out.print("Please specify package info: "+
                          "\nitem name: ");
         String itemName = stdin.readLine().trim();
         System.out.println(itemName);
 
-        b.addItem(itemName);
-        System.out.printf("Item %s inserted onto"+
-                          " the top of the stack.%n%n", itemName);
+        double itemWeight = readDouble("item weight: ");
+        int numItems = readInt("number of items: ");
+
+        System.out.print("is this a healthy item?(Y/N): ");
+        char healthy = 
+        (stdin.readLine().toUpperCase().trim().charAt(0));
+        System.out.println(healthy);
+
+        System.out.print("sender: ");
+        String sender = stdin.readLine().trim();
+        System.out.println(sender);
+
+        System.out.print("reciever: ");
+        String reciever = stdin.readLine().trim();
+        System.out.println(reciever);
+
+        try{
+            b.addItem(new Package(itemName, itemWeight, 
+            numItems, healthy == 'Y', sender, reciever));
+
+            System.out.printf("A package of %ss, each weighing %.2f"+
+                " lbs are now in the delivery bag.%n%n", itemName, itemWeight);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void dropOrder(Bag<String> stack) {
