9f01814c1473f36385c0ef00f3d4d224fb42b866
ajaxromik <awillcarr@gmail.com>
2023-10-16 10:41:35 -0400

    cleaned file tree

--- test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        Package p = new Package("bubs", 0.5, 5, false, "bubba", "bobby");
-        StackInterface<Package> si = new StackRA<>();
-        Bag<Package> b = new Bag<>(si);
-        b.addItem(p);
-        System.out.println(b);
-    }
-}
